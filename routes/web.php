4ceae04ec59de8238f5550664de93e99f30da13e
Jack Cheng <jack.cheng@piercer-tech.com>
2022-12-26 15:27:18 +0800

    try again

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    return view('welcome');
-    return redirect('/booking');
+    return view('welcome');
+//    return redirect('/booking');
+//    return redirect('/booking');
 });
 
 Route::get('/booking', function () {
