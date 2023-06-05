6b9a2d3da38825551b7294e9caff11384c42cf6b
Jack Cheng <jack.cheng@piercer-tech.com>
2023-06-05 14:42:40 +0800

    fix faq search query condition

--- app/Http/Livewire/ShowFrequentlyAnsweredQuestions.php
+++ app/Http/Livewire/ShowFrequentlyAnsweredQuestions.php
@@ -1,33 +1,37 @@
 <?php
 
-namespace App\Http\Livewire;
+    namespace App\Http\Livewire;
 
-use App\Models\FrequentlyAskedQuestion;
-use Livewire\Component;
+    use App\Models\FrequentlyAskedQuestion;
+    use Livewire\Component;
 
-class ShowFrequentlyAnsweredQuestions extends Component
-{
-
-    public $keyword;
-    public $faqs;
-
-    public function mount($business)
+    class ShowFrequentlyAnsweredQuestions extends Component
     {
-        $this->business = $business;
-        $this->faqs = FrequentlyAskedQuestion::where('business_id', $business->id)->get();
-    }
 
-    public function render()
-    {
-        return view('livewire.show-frequently-answered-questions');
-    }
+        public $keyword;
+        public $faqs;
 
-    public function search()
-    {
-        if ($this->keyword != '') {
-            $this->faqs = FrequentlyAskedQuestion::where('business_id', $this->business->id)->where('question', 'like', '%' . $this->keyword . '%')->orWhere('answer', 'like', '%' . $this->keyword . '%')->get();
-        }else{
-            $this->faqs = FrequentlyAskedQuestion::where('business_id', $this->business->id)->get();
+        public function mount($business)
+        {
+            $this->business = $business;
+            $this->faqs = FrequentlyAskedQuestion::where('business_id', $business->id)->get();
+        }
+
+        public function render()
+        {
+            return view('livewire.show-frequently-answered-questions');
+        }
+
+        public function search()
+        {
+            if ($this->keyword != '') {
+                $this->faqs = FrequentlyAskedQuestion::where('business_id', $this->business->id)->where(
+                    function ($query) {
+                        $query->where('question', 'like', '%' . $this->keyword . '%')->orWhere('answer', 'like', '%' . $this->keyword . '%');
+                    }
+                )->get();
+            } else {
+                $this->faqs = FrequentlyAskedQuestion::where('business_id', $this->business->id)->get();
+            }
         }
     }
-}
