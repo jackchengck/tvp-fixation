c63a7d4edd34879f637796372075b43b66d518e6
Jack Cheng <jack.cheng@piercer-tech.com>
2023-07-04 14:22:55 +0800

    try added get workard, store booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -57,22 +57,22 @@
         public function getStoreBooking(Request $request)
         {
 
-            dd($request);
-
-            $booking = new Booking();
-            $booking->order_num = $request->order_num;
-            $booking->customer_name = $request->customer_name;
-            $booking->customer_email = $request->customer_email;
-            $booking->customer_phone = '+852' . $request->customer_phone;
-            $booking->customer_password = $request->customer_password;
-            $booking->booking_date = $request->booking_date;
-            $booking->booking_time = $request->timeslot;
-            $booking->service_id = $request->service;
-            $booking->business_id = Service::find($request->service)->business->id;
-
-            $booking->save();
-
-            Mail::to($booking->customer_email)->send(new BookingCreatedToCustomer($booking));
+//            dd($request);
+//
+//            $booking = new Booking();
+//            $booking->order_num = $request->order_num;
+//            $booking->customer_name = $request->customer_name;
+//            $booking->customer_email = $request->customer_email;
+//            $booking->customer_phone = '+852' . $request->customer_phone;
+//            $booking->customer_password = $request->customer_password;
+//            $booking->booking_date = $request->booking_date;
+//            $booking->booking_time = $request->timeslot;
+//            $booking->service_id = $request->service;
+//            $booking->business_id = Service::find($request->service)->business->id;
+//
+//            $booking->save();
+//
+//            Mail::to($booking->customer_email)->send(new BookingCreatedToCustomer($booking));
 
             return redirect('booking')->with('status', 'Booking Has been created');
         }
