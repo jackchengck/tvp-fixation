bac9bc6c5ddca700d556f662969db063b1fd5a2c
Jack Cheng <jack.cheng@piercer-tech.com>
2023-07-24 18:47:03 +0800

    added product barcode search logic to inventory log

--- app/Http/Controllers/Admin/InventoryLogCrudController.php
+++ app/Http/Controllers/Admin/InventoryLogCrudController.php
@@ -1,182 +1,228 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+    namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\InventoryLogRequest;
-use App\Models\Location;
-use App\Models\Product;
-use App\Models\User;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
-use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
-use Backpack\ReviseOperation\ReviseOperation;
-use Carbon\Carbon;
-
-/**
- * Class InventoryLogCrudController
- * @package App\Http\Controllers\Admin
- * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
- */
-class InventoryLogCrudController extends CrudController
-{
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
-    use ReviseOperation;
+    use App\Http\Requests\InventoryLogRequest;
+    use App\Models\Location;
+    use App\Models\Product;
+    use App\Models\User;
+    use Backpack\CRUD\app\Http\Controllers\CrudController;
+    use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+    use Backpack\ReviseOperation\ReviseOperation;
+    use Carbon\Carbon;
 
     /**
-     * Configure the CrudPanel object. Apply settings to all operations.
-     *
-     * @return void
+     * Class InventoryLogCrudController
+     * @package App\Http\Controllers\Admin
+     * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
      */
-    public function setup()
+    class InventoryLogCrudController extends CrudController
     {
-        CRUD::setModel(\App\Models\InventoryLog::class);
-        CRUD::setRoute(config('backpack.base.route_prefix') . '/inventory-log');
-        CRUD::setEntityNameStrings('inventory log', 'inventory logs');
+        use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+        use ReviseOperation;
+
+        /**
+         * Configure the CrudPanel object. Apply settings to all operations.
+         *
+         * @return void
+         */
+        public function setup()
+        {
+            CRUD::setModel(\App\Models\InventoryLog::class);
+            CRUD::setRoute(config('backpack.base.route_prefix') . '/inventory-log');
+            CRUD::setEntityNameStrings('inventory log', 'inventory logs');
 
 
-        $this->crud->denyAccess(['create', 'delete', 'list', 'update']);
+            $this->crud->denyAccess(
+                [
+                    'create',
+                    'delete',
+                    'list',
+                    'update'
+                ]
+            );
 
-        if (backpack_user()->isSuperAdmin) {
-            $this->crud->allowAccess(['create', 'delete', 'list', 'update']);
+            if (backpack_user()->isSuperAdmin) {
+                $this->crud->allowAccess(
+                    [
+                        'create',
+                        'delete',
+                        'list',
+                        'update'
+                    ]
+                );
+            }
+
+
+            if (backpack_user()->hasPermissionTo('view inventory logs')) {
+                $this->crud->allowAccess(['list',]);
+            }
+
+            if (backpack_user()->hasPermissionTo('edit inventory logs')) {
+                $this->crud->allowAccess(
+                    [
+                        'list',
+                        'update'
+                    ]
+                );
+            }
+
+            if (backpack_user()->hasPermissionTo('create inventory logs')) {
+                $this->crud->allowAccess(['create']);
+            }
+            if (backpack_user()->hasPermissionTo('delete inventory logs')) {
+                $this->crud->allowAccess(['delete',]);
+            }
         }
 
+        /**
+         * Define what happens when the List operation is loaded.
+         *
+         * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+         * @return void
+         */
+        protected function setupListOperation()
+        {
 
-        if (backpack_user()->hasPermissionTo('view inventory logs')) {
-            $this->crud->allowAccess(['list',]);
-        }
+//            CRUD::column('product_id');
+            CRUD::addColumn(
+                [
+                    'name'        => 'product',
+                    'type'        => 'relationship',
+                    'searchLogic' => function ($query, $column, $searchTerm) {
+                        $query->orWhereHas(
+                            'product', function ($q) use ($column, $searchTerm) {
+                            $q->where('barcode', 'like', '%' . $searchTerm . '%')
+                                ->orWhere('title', 'like', '%' . $searchTerm . '%');
+                        }
+                        );
+                    }
 
-        if (backpack_user()->hasPermissionTo('edit inventory logs')) {
-            $this->crud->allowAccess(['list', 'update']);
-        }
-
-        if (backpack_user()->hasPermissionTo('create inventory logs')) {
-            $this->crud->allowAccess(['create']);
-        }
-        if (backpack_user()->hasPermissionTo('delete inventory logs')) {
-            $this->crud->allowAccess(['delete',]);
-        }
-    }
-
-    /**
-     * Define what happens when the List operation is loaded.
-     *
-     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
-     * @return void
-     */
-    protected function setupListOperation()
-    {
-
-        CRUD::column('product_id');
-        CRUD::column('location');
-        CRUD::addColumn([
-            'name' => 'type',
-            'type' => 'enum',
-            'options' => [
-                'move_in' => 'Move In',
-                'move_out' => 'Move out',
-                'consume' => 'Consumed',
-                'damaged' => 'Damaged',
-                'sold' => 'Sold'
-            ]
-        ]);
-        CRUD::column('quantity');
-        CRUD::column('remark');
+                ]
+            );
+            CRUD::column('location');
+            CRUD::addColumn(
+                [
+                    'name'    => 'type',
+                    'type'    => 'enum',
+                    'options' => [
+                        'move_in'  => 'Move In',
+                        'move_out' => 'Move out',
+                        'consume'  => 'Consumed',
+                        'damaged'  => 'Damaged',
+                        'sold'     => 'Sold'
+                    ]
+                ]
+            );
+            CRUD::column('quantity');
+            CRUD::column('remark');
 //        CRUD::column('business_id');
-        CRUD::column('user_id');
+            CRUD::column('user_id');
+
+            /**
+             * Columns can be defined using the fluent syntax or array syntax:
+             * - CRUD::column('price')->type('number');
+             * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
+             */
+
+
+            $this->crud->addFilter(
+                [
+                    'name'  => 'product',
+                    'type'  => 'select2',
+                    'label' => 'Product'
+                ], function () {
+                return Product::all()->keyBy('id')->pluck('title', 'id')->toArray();
+            }, function ($value) {
+                $this->crud->addClause('where', 'product_id', $value);
+            }
+            );
+
+
+            $this->crud->addFilter(
+                [
+                    'name'  => 'location',
+                    'type'  => 'select2',
+                    'label' => 'Location'
+                ], function () {
+                return Location::all()->keyBy('id')->pluck('title', 'id')->toArray();
+            }, function ($value) {
+                $this->crud->addClause('where', 'location_id', $value);
+            }
+            );
+
+            $this->crud->addFilter(
+                [
+                    'name'  => 'user',
+                    'type'  => 'select2',
+                    'label' => 'User'
+                ], function () {
+                return User::all()->keyBy('id')->pluck('name', 'id')->toArray();
+            }, function ($value) {
+                $this->crud->addClause('where', 'user_id', $value);
+            }
+            );
+        }
 
         /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
+         * Define what happens when the Create operation is loaded.
+         *
+         * @see https://backpackforlaravel.com/docs/crud-operation-create
+         * @return void
          */
+        protected function setupCreateOperation()
+        {
 
+            CRUD::setValidation(InventoryLogRequest::class);
 
-        $this->crud->addFilter([
-            'name' => 'product',
-            'type' => 'select2',
-            'label' => 'Product'
-        ], function () {
-            return Product::all()->keyBy('id')->pluck('title', 'id')->toArray();
-        }, function ($value) {
-            $this->crud->addClause('where', 'product_id', $value);
-        });
+            CRUD::field('type');
+            CRUD::addField(
+                [
+                    'name'    => 'type',
+                    'type'    => 'enum',
+                    'options' => [
+                        'move_in'  => 'Move In',
+                        'move_out' => 'Move out',
+                        'consume'  => 'Consumed',
+                        'damaged'  => 'Damaged',
+                        'sold'     => 'Sold'
+                    ]
+                ]
+            );
+            CRUD::field('quantity');
+            CRUD::field('remark');
+            CRUD::field('location_id');
+            CRUD::field('product_id');
+            CRUD::field('business_id');
+            CRUD::field('user_id');
+            CRUD::addField(
+                [
+                    'name'    => 'created_at',
+                    'type'    => 'datetime',
+                    'label'   => 'Created At',
+                    'default' => Carbon::now()->setTimezone('+8'),
+                ]
+            );
 
-
-        $this->crud->addFilter([
-            'name' => 'location',
-            'type' => 'select2',
-            'label' => 'Location'
-        ], function () {
-            return Location::all()->keyBy('id')->pluck('title', 'id')->toArray();
-        }, function ($value) {
-            $this->crud->addClause('where', 'location_id', $value);
-        });
-
-        $this->crud->addFilter([
-            'name' => 'user',
-            'type' => 'select2',
-            'label' => 'User'
-        ], function () {
-            return User::all()->keyBy('id')->pluck('name', 'id')->toArray();
-        }, function ($value) {
-            $this->crud->addClause('where', 'user_id', $value);
-        });
-    }
-
-    /**
-     * Define what happens when the Create operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-create
-     * @return void
-     */
-    protected function setupCreateOperation()
-    {
-
-        CRUD::setValidation(InventoryLogRequest::class);
-
-        CRUD::field('type');
-        CRUD::addField([
-            'name' => 'type',
-            'type' => 'enum',
-            'options' => [
-                'move_in' => 'Move In',
-                'move_out' => 'Move out',
-                'consume' => 'Consumed',
-                'damaged' => 'Damaged',
-                'sold' => 'Sold'
-            ]
-        ]);
-        CRUD::field('quantity');
-        CRUD::field('remark');
-        CRUD::field('location_id');
-        CRUD::field('product_id');
-        CRUD::field('business_id');
-        CRUD::field('user_id');
-        CRUD::addField([
-            'name' => 'created_at',
-            'type' => 'datetime',
-            'label' => 'Created At',
-            'default' => Carbon::now()->setTimezone('+8'),
-        ]);
+            /**
+             * Fields can be defined using the fluent syntax or array syntax:
+             * - CRUD::field('price')->type('number');
+             * - CRUD::addField(['name' => 'price', 'type' => 'number']));
+             */
+        }
 
         /**
-         * Fields can be defined using the fluent syntax or array syntax:
-         * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
+         * Define what happens when the Update operation is loaded.
+         *
+         * @see https://backpackforlaravel.com/docs/crud-operation-update
+         * @return void
          */
+        protected function setupUpdateOperation()
+        {
+            $this->setupCreateOperation();
+        }
     }
-
-    /**
-     * Define what happens when the Update operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-update
-     * @return void
-     */
-    protected function setupUpdateOperation()
-    {
-        $this->setupCreateOperation();
-    }
-}
