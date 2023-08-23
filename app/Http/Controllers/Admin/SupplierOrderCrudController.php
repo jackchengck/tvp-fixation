6268bb57449a720a6b2c0ccb007399a3b1043271
Jack Cheng <jack.cheng@piercer-tech.com>
2023-08-23 13:16:33 +0800

    added total_cost to supplier order admin list

--- app/Http/Controllers/Admin/SupplierOrderCrudController.php
+++ app/Http/Controllers/Admin/SupplierOrderCrudController.php
@@ -1,150 +1,185 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+    namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\SupplierOrderRequest;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
-use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
-use Backpack\ReviseOperation\ReviseOperation;
-
-/**
- * Class SupplierOrderCrudController
- * @package App\Http\Controllers\Admin
- * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
- */
-class SupplierOrderCrudController extends CrudController
-{
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
-    use ReviseOperation;
+    use App\Http\Requests\SupplierOrderRequest;
+    use Backpack\CRUD\app\Http\Controllers\CrudController;
+    use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+    use Backpack\ReviseOperation\ReviseOperation;
 
     /**
-     * Configure the CrudPanel object. Apply settings to all operations.
-     *
-     * @return void
+     * Class SupplierOrderCrudController
+     * @package App\Http\Controllers\Admin
+     * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
      */
-    public function setup()
+    class SupplierOrderCrudController extends CrudController
     {
-        CRUD::setModel(\App\Models\SupplierOrder::class);
-        CRUD::setRoute(config('backpack.base.route_prefix') . '/supplier-order');
-        CRUD::setEntityNameStrings('supplier order', 'supplier orders');
-
-
-        $this->crud->denyAccess(['create', 'delete', 'list', 'update']);
-
-        if (backpack_user()->isSuperAdmin) {
-            $this->crud->allowAccess(['create', 'delete', 'list', 'update']);
-        }
-
-
-        if (backpack_user()->hasPermissionTo('view supplier orders')) {
-            $this->crud->allowAccess(['list',]);
-        }
-
-        if (backpack_user()->hasPermissionTo('edit supplier orders')) {
-            $this->crud->allowAccess(['list', 'update']);
-        }
-
-        if (backpack_user()->hasPermissionTo('create supplier orders')) {
-            $this->crud->allowAccess(['create']);
-        }
-        if (backpack_user()->hasPermissionTo('delete supplier orders')) {
-            $this->crud->allowAccess(['delete',]);
-        }
-    }
-
-    /**
-     * Define what happens when the List operation is loaded.
-     *
-     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
-     * @return void
-     */
-    protected function setupListOperation()
-    {
-//        CRUD::column('id');
-        CRUD::column('supplier_id');
-        CRUD::addColumn([
-            'name' => 'supplierOrderItems',
-            'type' => 'relationship',
-//            'label'=>
-            'attribute' => 'title',
-        ]);
-        CRUD::column('user_id');
-        CRUD::column('business_id');
-
-        $this->crud->addButtonFromModelFunction('line', 'get_supplier_order_email_button', 'getSupplierOrderEmailButton', 'beginning');
+        use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+        use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+        use ReviseOperation;
 
         /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
+         * Configure the CrudPanel object. Apply settings to all operations.
+         *
+         * @return void
          */
-    }
-
-    /**
-     * Define what happens when the Create operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-create
-     * @return void
-     */
-    protected function setupCreateOperation()
-    {
-        CRUD::setValidation(SupplierOrderRequest::class);
-
-        CRUD::field('supplier_id');
-        CRUD::field('user_id');
-        CRUD::field('business_id');
-
-        CRUD::addFields([
-            [
-                'name' => 'supplierOrderItems',
-                'type' => 'relationship',
+        public function setup()
+        {
+            CRUD::setModel(\App\Models\SupplierOrder::class);
+            CRUD::setRoute(config('backpack.base.route_prefix') . '/supplier-order');
+            CRUD::setEntityNameStrings('supplier order', 'supplier orders');
 
 
-                'subfields' => [
+            $this->crud->denyAccess(
+                [
+                    'create',
+                    'delete',
+                    'list',
+                    'update'
+                ]
+            );
+
+            if (backpack_user()->isSuperAdmin) {
+                $this->crud->allowAccess(
                     [
-                        'name' => 'product',
-                        'type' => 'relationship',
+                        'create',
+                        'delete',
+                        'list',
+                        'update'
+                    ]
+                );
+            }
 
 
-//                'entity' => 'maids',
-//                        'attribute' => 'name',
-//                'data_source' => url('api/maid'),
-//                'pivot' => true,
-//                        'include_all_form_fields' => true,
-//                        'method' => 'POST',
+            if (backpack_user()->hasPermissionTo('view supplier orders')) {
+                $this->crud->allowAccess(['list',]);
+            }
+
+            if (backpack_user()->hasPermissionTo('edit supplier orders')) {
+                $this->crud->allowAccess(
+                    [
+                        'list',
+                        'update'
+                    ]
+                );
+            }
+
+            if (backpack_user()->hasPermissionTo('create supplier orders')) {
+                $this->crud->allowAccess(['create']);
+            }
+            if (backpack_user()->hasPermissionTo('delete supplier orders')) {
+                $this->crud->allowAccess(['delete',]);
+            }
+        }
+
+        /**
+         * Define what happens when the List operation is loaded.
+         *
+         * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+         * @return void
+         */
+        protected function setupListOperation()
+        {
+//        CRUD::column('id');
+            CRUD::column('supplier_id');
+            CRUD::addColumns(
+                [
+                    [
+                        'name'      => 'supplierOrderItems',
+                        'type'      => 'relationship',
+                        //            'label'=>
+                        'attribute' => 'title',
                     ],
                     [
-                        'name' => 'quantity',
-                        'type' => 'number',
+                        'name'       => 'total_cost',
+                        'type'       => 'number',
                         'attributes' => [
                             'step' => 'any'
                         ],
-                        'default' => 0,
-                    ],
+                        'default'    => 0,
+                        'prefix'     => '$',
 
+                    ]
                 ]
-            ]
-        ]);
+            );
+            CRUD::column('user_id');
+            CRUD::column('business_id');
+
+            $this->crud->addButtonFromModelFunction('line', 'get_supplier_order_email_button', 'getSupplierOrderEmailButton', 'beginning');
+
+            /**
+             * Columns can be defined using the fluent syntax or array syntax:
+             * - CRUD::column('price')->type('number');
+             * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
+             */
+        }
 
         /**
-         * Fields can be defined using the fluent syntax or array syntax:
-         * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
+         * Define what happens when the Create operation is loaded.
+         *
+         * @see https://backpackforlaravel.com/docs/crud-operation-create
+         * @return void
          */
-    }
+        protected function setupCreateOperation()
+        {
+            CRUD::setValidation(SupplierOrderRequest::class);
 
-    /**
-     * Define what happens when the Update operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-update
-     * @return void
-     */
-    protected function setupUpdateOperation()
-    {
-        $this->setupCreateOperation();
+            CRUD::field('supplier_id');
+            CRUD::field('user_id');
+            CRUD::field('business_id');
+
+            CRUD::addFields(
+                [
+                    [
+                        'name' => 'supplierOrderItems',
+                        'type' => 'relationship',
+
+
+                        'subfields' => [
+                            [
+                                'name' => 'product',
+                                'type' => 'relationship',
+
+
+                                //                'entity' => 'maids',
+                                //                        'attribute' => 'name',
+                                //                'data_source' => url('api/maid'),
+                                //                'pivot' => true,
+                                //                        'include_all_form_fields' => true,
+                                //                        'method' => 'POST',
+                            ],
+                            [
+                                'name'       => 'quantity',
+                                'type'       => 'number',
+                                'attributes' => [
+                                    'step' => 'any'
+                                ],
+                                'default'    => 0,
+                            ],
+
+                        ]
+                    ]
+                ]
+            );
+
+            /**
+             * Fields can be defined using the fluent syntax or array syntax:
+             * - CRUD::field('price')->type('number');
+             * - CRUD::addField(['name' => 'price', 'type' => 'number']));
+             */
+        }
+
+        /**
+         * Define what happens when the Update operation is loaded.
+         *
+         * @see https://backpackforlaravel.com/docs/crud-operation-update
+         * @return void
+         */
+        protected function setupUpdateOperation()
+        {
+            $this->setupCreateOperation();
+        }
     }
-}
--- app/Models/SupplierOrder.php
+++ app/Models/SupplierOrder.php
@@ -1,63 +1,78 @@
 <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use App\Traits\MultiTenantable;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Venturecraft\Revisionable\RevisionableTrait;
+    use App\Traits\MultiTenantable;
+    use Illuminate\Database\Eloquent\Builder;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+    use Venturecraft\Revisionable\RevisionableTrait;
 
-class SupplierOrder extends Model
-{
-    use MultiTenantable;
-    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
-    use HasFactory;
-    use RevisionableTrait;
-
-    public function identifiableName()
+    class SupplierOrder extends Model
     {
-        return $this->id;
+        use MultiTenantable;
+        use \Backpack\CRUD\app\Models\Traits\CrudTrait;
+        use HasFactory;
+        use RevisionableTrait;
+
+        public function identifiableName()
+        {
+            return $this->id;
+        }
+
+        // If you are using another bootable trait
+        // be sure to override the boot method in your model
+        public static function boot()
+        {
+            parent::boot();
+        }
+
+        protected $fillable = [
+            'supplier_id',
+            'user_id',
+            'business_id'
+        ];
+
+
+        protected $appends = [
+            'total_cost',
+        ];
+
+
+        public function business()
+        {
+            return $this->belongsTo(Business::class);
+        }
+
+        public function user()
+        {
+            return $this->belongsTo(User::class);
+        }
+
+        public function supplier()
+        {
+            return $this->belongsTo(Supplier::class);
+        }
+
+        public function supplierOrderItems()
+        {
+            return $this->hasMany(SupplierOrderItem::class);
+        }
+
+        public function getSupplierOrderEmailButton()
+        {
+            return '<a class="btn btn-sm btn-link" target="_blank" href=' . backpack_url("mail/send-supplier-order-email") . "/" . $this->id . ' data-toggle="tooltip" title="Supplier Order Email"><i class="la la-envelope"></i> Supplier Order Email</a>';
+        }
+
+        public function getTotalCostAttribute()
+        {
+            $items = $this->supplierOrderItems;
+            $totalCost = 0;
+            foreach ($items as $item) {
+                $totalCost += $item->quantity * $item->product->cost;
+            }
+
+            return $totalCost;
+        }
+
     }
-
-    // If you are using another bootable trait
-    // be sure to override the boot method in your model
-    public static function boot()
-    {
-        parent::boot();
-    }
-
-    protected $fillable = [
-        'supplier_id',
-        'user_id',
-        'business_id'
-    ];
-
-
-    public function business()
-    {
-        return $this->belongsTo(Business::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function supplier()
-    {
-        return $this->belongsTo(Supplier::class);
-    }
-
-    public function supplierOrderItems()
-    {
-        return $this->hasMany(SupplierOrderItem::class);
-    }
-
-    public function getSupplierOrderEmailButton()
-    {
-        return '<a class="btn btn-sm btn-link" target="_blank" href=' . backpack_url("mail/send-supplier-order-email") . "/" . $this->id . ' data-toggle="tooltip" title="Supplier Order Email"><i class="la la-envelope"></i> Supplier Order Email</a>';
-    }
-
-
-}
