3bc2380e58dce9735f1fd699478268c3f78bace7
Jack Cheng <jack.cheng@piercer-tech.com>
2023-11-15 17:25:00 +0800

    fixed sms, deleted sender

--- app/Notifications/BookingCreatedSMS.php
+++ app/Notifications/BookingCreatedSMS.php
@@ -70,9 +70,8 @@
         {
             return SnsMessage::create(
                 [
-                    'body'          => 'Your booking on ' . $this->booking->booking_date . " " . $this->booking->booking_time . " has been confirmed.\nWallet passcode: " . $this->booking->customer_password . "\n閣下 " . $this->booking->booking_date . " " . $this->booking->booking_time . "的預約已經確認。\n錢包密碼： " . $this->booking->customer_password,
+                    'body'          => $this->booking->business->title.'Smart Notification 智能提示:\n'.'Your booking on ' . $this->booking->booking_date . " " . $this->booking->booking_time . " has been confirmed.\nWallet passcode: " . $this->booking->customer_password . "\n閣下 " . $this->booking->booking_date . " " . $this->booking->booking_time . "的預約已經確認。\n錢包密碼： " . $this->booking->customer_password,
                     'transactional' => true,
-                    'sender' => $this->booking->business->title,
 
                 ]
             );
