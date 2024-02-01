c516cdfd6f0ecf1f3bfb2609d628ae36ccd5def9
Jack Cheng <jack.cheng@piercer-tech.com>
2024-02-01 13:44:00 +0800

    xframe middleware

--- app/Http/Middleware/FrameHeadersMiddleware.php
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -18,12 +18,28 @@
         {
             $response = $next($request);
 
-            $getHost = request()->getHost();
+            $dict = [
+                'jnjtrading.epochal-tech-hk.com'     => 'https://jjtrading.ibusiness.com.hk/',
+                'jnjtrading.localhost'               => 'https://jjtrading.ibusiness.com.hk/test',
+                'tastydiner.futurustechshk.com'      => 'https://tastydiner.ibusiness.com.hk/',
+                'kobayashiramen.epochal-tech-hk.com' => 'https://kobayashi.ibusiness.com.hk/',
+                'guardwell.gosmartltdhk.com'         => 'https://guardwellmall.ibusiness.com.hk/',
+            ];
 
-            if ($getHost == 'jnjtrading.epochal-tech-hk.com') {
-//            if ($getHost == 'jnjtrading.localhost') {
-                $response->header('X-Frame-Options', 'ALLOW FROM https://jjtrading.ibusiness.com.hk/');
+
+            $getHost = request()->getHost();
+            foreach ($dict as $key => $value) {
+                if ($getHost == $key) {
+//                $response->header('X-Frame-Options', 'ALLOW FROM https://jjtrading.ibusiness.com.hk/');
+                    $response->headers->set('X-Frame-Options', 'ALLOW FROM ' . $value, true);
+                }
             }
+
+//            if ($getHost == 'jnjtrading.epochal-tech-hk.com') {
+////            if ($getHost == 'jnjtrading.localhost') {
+////                $response->header('X-Frame-Options', 'ALLOW FROM https://jjtrading.ibusiness.com.hk/');
+//                $response->headers->set('X-Frame-Options', 'ALLOW FROM https://jjtrading.ibusiness.com.hk/', true);
+//            }
 
             return $response;
         }
